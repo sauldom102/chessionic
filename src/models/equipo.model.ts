4d39a8830ec93819064ad41cc62043f2db930cbe
Saul Dominguez <saulydominguez@gmail.com>
2018-11-15 12:50:57 +0100

    Equipo model changed

--- src/models/equipo.model.ts
+++ src/models/equipo.model.ts
@@ -1,16 +1,24 @@
 import { Jugador } from './jugador.model';
 
+import { JugadoresProvider } from '../providers/jugadores/jugadores'
 
 export default class Equipo{
     
     id:string
-    jugadores:Jugador
+    jugadores:Jugador[]
 
-    constructor(id:string, jugadores:Jugador){
+    constructor(id:string, jugadores:Jugador[]){
         this.id=id
         this.jugadores=jugadores
+	}
 
-    }
+	addJugador(jugador:Jugador){
+		this.jugadores.push(jugador)
+	}
+
+	addJugadorById(jugadorId:number){
+		this.addJugador(new JugadoresProvider().cargar_jugadores().find(jugador => jugador.id == jugadorId))
+	}
 }
 
 
